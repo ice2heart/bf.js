3d65e86246d341b6bd882c93fc1f3966f534ce4a
A.G. Yakupov <ag.yakupov@tensor.ru>
2017-02-28 17:20:32 +0500

    add readme

---
+++ README.md
@@ -0,0 +1,50 @@
+# Bf.js [![Code Climate](https://lima.codeclimate.com/github/ice2heart/bf.js/badges/gpa.svg)](https://lima.codeclimate.com/github/ice2heart/bf.js) [![Issue Count](https://lima.codeclimate.com/github/ice2heart/bf.js/badges/issue_count.svg)](https://lima.codeclimate.com/github/ice2heart/bf.js)
+## Yet another brainfuck interpreter and text2bf
+
+Simple bf interpreter and text to bf code app. It's require node.js >= 6.9.0
+
+## How to install
+
+```
+git clone git@github.com:ice2heart/bf.js.git; cd bf.js
+```
+
+## How to use interpreter
+
+Simple use:
+
+```
+./bf.js test/Beer.b
+```
+
+Options:
+
+-e Evaluate script
+```
+./bf.js -e='-[----->+<]>--.+.+.'
+```
+
+--cycle limit execute cycles:
+```
+./bf.js --cycle=30000 test/Beer.b
+```
+
+## How to use text2bf
+
+```
+./text2bf.js 'Hello world'
+```
+
+Default output file is 'out.b'
+
+-o output filename
+
+```
+./text2bf.js 'Hello world' -o test.b
+```
+
+-f input filename
+
+```
+./text2bf.js -f text.ascii
+```
--- bf.js
+++ bf.js
@@ -5,121 +5,133 @@ const argv = require('minimist')(process.argv.slice(2));
 const prompt = require('prompt-sync')();
 
 class BfExec {
-  constructor() {
+  constructor(program, maxCycleCount) {
+    this.mem = new Buffer(3000000);
+    this.mem.fill(0);
 
+    this.position = 0;
+    this.exec = 0;
+    this.rangeStack = [];
+    this.maxCount = maxCycleCount;
+    this.cycleCount = 0;
+    this.prog = program;
   }
-}
 
-const mem = new Buffer(3000000);
-mem.fill(0);
-
-var position = 0;
-var exec = 0;
-const rangeStack = [];
-const execute = argv.e;
-var prog = '';
-if (!execute) {
-  prog = fs.readFileSync(argv._[0]).toString();
-} else {
-  prog = execute;
-}
-const maxCount = argv.cycle;
-var cycleCount = 0;
-
-const prettyPrint = function() {
-  console.log("Exec pos ", exec);
-  if (position > 6) {
-    console.log("Mem is", mem[position], " ", mem.slice(position - 5, position + 5));
-  } else {
-    console.log("Mem is", mem[position], " ", mem.slice(0, 10));
+  prettyPrint() {
+    console.log("Exec pos ", this.exec);
+    if (this.position > 6) {
+      console.log("Mem is", this.mem[this.position], " ", this.mem.slice(this.position - 5, this.position + 5));
+    } else {
+      console.log("Mem is", this.mem[this.position], " ", this.mem.slice(0, 10));
+    }
+    if (this.exec > 6) {
+      console.log("Prog ", this.exec, this.prog[this.exec], "text:", this.prog.slice(this.exec - 5, this.exec + 5));
+    } else {
+      console.log("Prog ", this.exec, this.prog[this.exec], "text:", this.prog.slice(0, 10));
+    }
   }
-  if (exec > 6) {
-    console.log("Prog ", exec, prog[exec], "text:", prog.slice(exec - 5, exec + 5));
-  } else {
-    console.log("Prog ", exec, prog[exec], "text:", prog.slice(0, 10));
-  }
-  console.log("");
-};
 
-while (exec < prog.length) {
-  //prettyPrint();
-  if (maxCount && maxCount < cycleCount)
-    break;
-  switch (prog[exec]) {
+  doCycle() {
+    if (this.maxCount && this.maxCount < this.cycleCount)
+      return 1;
+    switch (this.prog[this.exec]) {
     case '+':
-      mem[position]++;
+      this.mem[this.position]++;
       break;
     case '-':
-      mem[position]--;
+      this.mem[this.position]--;
       break;
     case '>':
-      position++;
+      this.position++;
       break;
     case '<':
-      position--;
+      this.position--;
       break;
     case '.':
-      process.stdout.write(String.fromCharCode(mem[position]));
+      process.stdout.write(String.fromCharCode(this.mem[this.position]));
       break;
     case '$':
-      process.stdout.write(String(mem[position]));
+      process.stdout.write(String(this.mem[this.position]));
       break;
     case ',':
       var p = prompt("");
       if (p.length === 0) {
-        mem[position] = 10;
+        this.mem[this.position] = 10;
       } else {
-        mem[position] = p.charCodeAt(0);
+        this.mem[this.position] = p.charCodeAt(0);
       }
       break;
     case '@':
-      prettyPrint();
+      this.prettyPrint();
       prompt("");
       break;
     case '[':
-      if (mem[position] === 0) {
+      if (this.mem[this.position] === 0) {
         var inner = 0;
         var found = true;
-        exec++;
+        this.exec++;
         while (found) {
-          if (prog[exec] === '[') {
+          if (this.prog[this.exec] === '[') {
             inner++;
           }
-          if (inner === 0 && prog[exec] === ']') {
+          if (inner === 0 && this.prog[this.exec] === ']') {
             found = false;
-          } else if (inner > 0 && prog[exec] === ']') {
+          } else if (inner > 0 && this.prog[this.exec] === ']') {
             inner--;
           }
 
-          if (exec > prog.length) {
+          if (this.exec > this.prog.length) {
             console.error('Don\'t close ]');
-            process.exit();
+            throw ('runtime error');
           }
-          exec++;
+          this.exec++;
         }
-        exec--;
+        this.exec--;
       } else {
-        rangeStack.push(exec);
+        this.rangeStack.push(this.exec);
       }
       break;
     case ']':
-      if (mem[position] === 0) {
-        rangeStack.pop();
+      if (this.mem[this.position] === 0) {
+        this.rangeStack.pop();
       } else {
-        if (rangeStack.length < 1) {
+        if (this.rangeStack.length < 1) {
           console.error('rangeStack.length < 1');
-          process.exit();
+          throw ('runtime error');
         }
-        exec = rangeStack[rangeStack.length - 1];
+        this.exec = this.rangeStack[this.rangeStack.length - 1];
       }
       break;
     default:
       break;
+    }
+    if (this.exec === undefined || this.position === undefined) {
+      console.error('Status carrupted', exec, position, rangeStack);
+      throw ('Status carrupted');
+    }
+    this.exec++;
+    this.cycleCount++;
+    return 0;
   }
-  if (exec === undefined || position === undefined) {
-    console.error('Status carrupted', exec, position, rangeStack);
-    process.exit();
+
+  execute() {
+    while (this.exec < this.prog.length) {
+      if (this.doCycle())
+        break;
+    }
   }
-  exec++;
-  ++cycleCount;
 }
+
+
+const execute = argv.e;
+var prog = '';
+if (execute) {
+  prog = execute;
+} else if (argv._[0]) {
+  prog = fs.readFileSync(argv._[0]).toString();
+} else {
+  console.error('No data');
+}
+
+const bf = new BfExec(prog, argv.cycle);
+bf.execute();
